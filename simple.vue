927fd935274f02d510091b8e302ee7fac8ae9fa0
Daniel Cherubini <daniel@cherubini.casa>
2018-02-13 11:18:43 +0100

    testing

---
+++ simple.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <p class="simple">Hello From Component in {{location}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+      return {
+          location: "node_modules"
+      }
+  }
+}
+</script>
+
+<style>
+.simple {
+    color: blue;
+    text-decoration: underline;
+}
+</style>
